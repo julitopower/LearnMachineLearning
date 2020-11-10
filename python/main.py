b04a61ef20700b39e9aca023b770ad552efd9ec0
julitopower <julio.delgadomangas@gmail.com>
2020-11-10 15:43:21 -0800

    Cifar-10 Keras-TF training script

---
+++ python/main.py
@@ -0,0 +1,39 @@
+import pickle as pk
+import numpy as np
+import tensorflow.keras as keras
+import tensorflow as tf
+
+"""Algorithm:
+1.- add batch files to list
+2.- For each train in-memory batch:
+    a. Unpickle
+    b. Call fit with certain batch size
+3. Divide test in-memory batch in validation/test
+4. Evaluate model
+"""
+
+def architecture():
+    model = keras.models.Sequential()
+    model.add(keras.layers.Input(shape=(32, 32, 3)))
+    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D((2, 2)))
+    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(keras.layers.MaxPool2D((2, 3)))
+    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(64, activation='relu'))
+    model.add(keras.layers.Dense(10))
+    return model
+            
+
+(X_train, y_train), (X_test, y_test) = keras.datasets.cifar10.load_data()
+
+model = architecture()
+model.summary()
+model.compile(optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=10,
+          validation_data=(X_test, y_test))
+
