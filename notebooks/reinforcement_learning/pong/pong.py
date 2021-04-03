2d68f264d2ff501d42f4879b014934bec11af05a
Julio Delgado Mangas <julio.delgadomangas@gmail.com>
2021-04-02 19:12:34 -0700

    Fixing rewards again

--- notebooks/reinforcement_learning/pong/pong.py
+++ notebooks/reinforcement_learning/pong/pong.py
@@ -86,11 +86,16 @@ class Pong():
         # We want to reward quickly going to the right final
         # location for the racket
         bally = self.state()[1]
-        steps_penalty = (abs(self.starty - bally) -  self.steps)
+        optimal_steps = abs(self.starty - bally)
+        if self.steps > optimal_steps:
+            steps_penalty = float(self.steps) / (optimal_steps + 1)
+        else:
+            steps_penalty = 1.0;
+            
         if r > 0:
-            return 100 + steps_penalty
+            return 100.0 / steps_penalty
         elif r < 0:
-            return r + steps_penalty
+            return r - steps_penalty
         else:
             return 0
 
@@ -100,70 +105,3 @@ class Pong():
     def render(self):
         self.lib.pong_render(self.game)
 
-
-def evaluate(iterations=10000):
-    game = Pong()
-    reward = 0
-    for _ in range(0, iterations):
-        game.reset()
-        while game.reward() == 0:
-            game.step(random.randint(0, 0))
-        if game.reward() == 1:
-            reward += 1
-    return 100 * reward / iterations
-
-
-if __name__ == "__main__":
-
-    parser = ap.ArgumentParser(description="Pong game RL")
-    parser.add_argument("--gamma", type=float, required=True)
-    parser.add_argument("--lr", help="Learning rate", type=float, required=True)
-    parser.add_argument("--er", help="Entropy rate", type=float, required=True)
-    parser.add_argument("-v", "--verbose", help="Visualize pong game", action="store_true", required=True)
-    args = parser.parse_args()
-
-    print(f"#######{args.gamma}")
-
-    # print(evaluate())
-    # Setup numpy print options
-    np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
-
-    gamma = args.gamma
-    lr = args.lr
-    entropy_c = args.er
-    render = args.v
-    score_history = []
-
-    agent = actorcriticagent2.DummyAgent(8, 3, gamma, lr, entropy_c,
-                                        #layers=[32, 10240, 256, 20000, 2560])
-                                        #layers=[32, 64, 1024, 2560, 20000, 2560, 1024, 1024, 512])
-                                        layers_actor=[64, 256, 512, 1024],
-                                        layers_critic=[64, 256, 64])
-    # agent.load("model__test.h5")
-    # agent.load('model___new_test.h5')
-    # agent.load('model_good_march_3rd.h5')
-    try:
-        agent.load('ac_03072021.h5')
-        print("######### Model loaded succesfully")
-    except:
-        pass
-    game = Pong(800, 600, int(400/2), int(200/2))
-    for i in range(0, 10000):
-        game.reset()
-        idx = 0
-        while not game.done():
-            state = game.state()
-            action, action_probs = agent.action(state)
-            game.step(action)
-            agent.record(state, action, action_probs, game.reward())
-            if idx % 50 == 0:
-                pass
-                # print(state, action, action_probs, game.reward())
-            idx += 1
-            if render:
-                game.render()
-        if i % 10 == 0:
-            agent.save("ac_03072021.h5")
-        score_history.append(game.reward())
-        print(f"iter0 {i}, reward: {score_history[-1]:.2f}", " -- Avg over 100 episodes", np.mean(score_history[-100:]))
-        agent.train()
